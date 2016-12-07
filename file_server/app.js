9d0af52b2a4e3e6dcc21106263a4b88fe95ce7af
aheifetz <aheifetz@mit.edu>
2016-12-06 20:10:10 -0500

    Stopped using express body parser, custom parsing instead

--- file_server/app.js
+++ file_server/app.js
@@ -1,34 +1,21 @@
 var express = require('express');
 var app = express();
 var path = require('path');
-var bodyParser = require('body-parser');
 
-app.use(bodyParser.raw({limit: '2000kb'}));
+var parseBody = function(req, res, next) {
+  var data = '';
+  req.setEncoding('utf8');
+  req.on('data', function(chunk) {
+    data += chunk;
+  });
+  req.on('end', function() {
+    req.body = data;
+    next();
+  });
+}
+app.use(parseBody);
 
-var curriedGetResponse = function(size) {
-  return function(req, res) {
-    var options = {
-      root: __dirname,
-      lastModified: false,
-      cacheControl: false,
-      headers: {
-        'Transfer-Encoding': 'identity',
-        'Content-Encoding': 'identity',
-        'Content-Length': sizeOctets,
-        'Access-Control-Allow-Origin': '*',
-      }
-    };
-    res.sendFile(size + 'kb.dat', options, function (err) {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log('Sent file of size ' + size + ' kb!');
-      }
-    });
-  }
-};
-
-var curriedPostResponse = function(size) {
+var handlePost = function(size) {
   sizeOctets = 0;
   switch(size) {
     case '16':
@@ -41,26 +28,22 @@ var curriedPostResponse = function(size) {
       sizeOctets = 131072;
   }
   return function(req, res) {
+    console.log("Received body " + size);
+    console.log(req.body);
     res.writeHead(200, {
       'Access-Control-Allow-Origin': '*',
       'Cache-Control': false,
       'Content-Length': sizeOctets,
-      'Access-Control-Allow-Origin': '*',
     });
     res.write(req.body);
     res.end();
   }
 }
 
-app.get('/get/16', curriedGetResponse('16'));
-app.get('/get/64', curriedGetResponse('64'));
-app.get('/get/256', curriedGetResponse('256'));
-app.get('/get/1024', curriedGetResponse('1024'));
-
-app.post('/post/16', curriedPostResponse('16'));
-app.post('/post/64', curriedPostResponse('64'));
-app.post('/post/256', curriedPostResponse('256'));
-app.post('/post/1024', curriedPostResponse('1024'));
+app.post('/post/16', handlePost('16'));
+app.post('/post/64', handlePost('64'));
+app.post('/post/256', handlePost('256'));
+app.post('/post/1024', handlePost('1024'));
 
 app.listen(3000, function() {
   console.log('File server listening on port 3000');
