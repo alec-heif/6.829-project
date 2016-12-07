9b8712540a5ecfd75cfa2c218f0dfa998b004f68
aheifetz <aheifetz@mit.edu>
2016-12-06 20:43:23 -0500

    Fixed bugs, server now logs buffer length

--- file_server/app.js
+++ file_server/app.js
@@ -3,21 +3,22 @@ var app = express();
 var path = require('path');
 
 var handlePost = function(req, res) {
-	var body = [];
-	req.on('data', function(chunk) {
-			body.push(chunk);
-			});
-	req.on('end', function() {
-			var realBody = Buffer.concat(body);
-			res.set('Access-Control-Allow-Origin', '*');
-			res.send(realBody);
-			});
+  var body = [];
+  req.on('data', function(chunk) {
+    body.push(chunk);
+  });
+  req.on('end', function() {
+    var realBody = Buffer.concat(body);
+    console.log("Buffer of length " + realBody.length);
+    res.set('Access-Control-Allow-Origin', '*');
+    res.send(realBody);
+  });
 }
 
-app.post('/post/16', handlePost();
-app.post('/post/64', handlePost();
-app.post('/post/256', handlePost();
-app.post('/post/1024', handlePost();
+app.post('/post/16', handlePost());
+app.post('/post/64', handlePost());
+app.post('/post/256', handlePost());
+app.post('/post/1024', handlePost());
 
 app.listen(3000, function() {
   console.log('File server listening on port 3000');
