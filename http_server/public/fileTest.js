fbb7f15b81cab0d2176126408e5d4828ea9b52c5
aheifetz <aheifetz@mit.edu>
2016-12-07 16:10:33 -0500

    Remove unnecessary buttons

--- http_server/public/fileTest.js
+++ http_server/public/fileTest.js
@@ -1,21 +1,29 @@
+baseUrl = 'http://23.236.48.150:3000/';
+
 function singleDownload(size) {
-  var url = "http://23.236.48.150:3000/" + size;
+  var url = baseUrl + "get/" + size;
   var onSuccess = function() {
     alert('success!');
   }
   $.ajax({
-    url: url,
+    url: toGet(size),
     success: onSuccess,
     cache: false
   });
 }
 
+function toGet(size) {
+  return baseUrl + "get/" + size;
+}
+function toPost(size) {
+  return baseUrl + "post/" + size;
+}
+
 function downloadAll() {
   var sizes = [16, 64, 256, 1024];
   sizes.forEach(size => {
-    var url = "http://23.236.48.150:3000/" + size;
     $.ajax({
-      url: url,
+      url: toGet(size),
       cache: false,
       async: false
     });
@@ -23,3 +31,35 @@ function downloadAll() {
   alert('Done!');
 }
 
+function singleUpload(size, cb) {
+  var url = size + "kb.dat";
+  var onSuccess = function(data) {
+    $.ajax({
+      url: toPost(size),
+      type: 'POST',
+      data: data,
+      processData: false,
+      success: cb
+    });
+  };
+  $.ajax({
+    url: url,
+    type: 'GET',
+    contentType: 'application/octet-stream',
+    processData: false,
+    success: onSuccess
+  });
+}
+
+function uploadAll() {
+  singleUpload(16, function() {
+    singleUpload(64, function() {
+      singleUpload(256, function() {
+        singleUpload(1024, function() {
+          console.log("Done uploading all");
+          alert("Experiment done!");
+        });
+      });
+    });
+  });
+}
