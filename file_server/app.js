189dc673f96474214437228d51a342bfea8e87d5
aheifetz <aheifetz@mit.edu>
2016-12-06 20:48:57 -0500

    Change buffer allocation to use length

--- file_server/app.js
+++ file_server/app.js
@@ -4,21 +4,23 @@ var path = require('path');
 
 var handlePost = function(req, res) {
   var body = [];
+  var length= 0;
   req.on('data', function(chunk) {
     body.push(chunk);
+    length += chunk.length;
   });
   req.on('end', function() {
-    var realBody = Buffer.concat(body);
+    var realBody = Buffer.concat(body, length);
     console.log("Buffer of length " + realBody.length);
     res.set('Access-Control-Allow-Origin', '*');
     res.send(realBody);
   });
 }
 
-app.post('/post/16', handlePost());
-app.post('/post/64', handlePost());
-app.post('/post/256', handlePost());
-app.post('/post/1024', handlePost());
+app.post('/post/16', handlePost);
+app.post('/post/64', handlePost);
+app.post('/post/256', handlePost);
+app.post('/post/1024', handlePost);
 
 app.listen(3000, function() {
   console.log('File server listening on port 3000');
