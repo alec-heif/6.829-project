17ad0a60826959a0bd57c4d0282d535906fd237c
aheifetz <aheifetz@mit.edu>
2016-12-07 22:22:17 -0500

    Restored get functionality to app, removed post response

--- file_server/app.js
+++ file_server/app.js
@@ -2,18 +2,45 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
+var curriedGetResponse = function(size) {
+  sizeOctets = 0;
+  switch(size) {
+    case '16':
+      sizeOctets = 2048;
+    case '64':
+      sizeOctets = 8192;
+    case '256':
+      sizeOctets = 32768;
+    case '1024':
+      sizeOctets = 131072;
+  }
+  return function(req, res) {
+    var options = {
+      root: __dirname,
+      lastModified: false,
+      cacheControl: false,
+      headers: {
+        'Content-Length': sizeOctets,
+        'Access-Control-Allow-Origin': '*',
+      }
+    };
+    res.sendFile(size + 'kb.dat', options, function (err) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log('Sent file of size ' + size + ' kb!');
+      }
+    });
+  }
+};
+
 var handlePost = function(req, res) {
   var body = [];
   var length= 0;
-  req.on('data', function(chunk) {
-    body.push(chunk);
-    length += chunk.length;
-  });
   req.on('end', function() {
-    var realBody = Buffer.concat(body, length);
-    console.log("Buffer of length " + realBody.length);
     res.set('Access-Control-Allow-Origin', '*');
-    res.send(realBody);
+    res.status(200);
+    res.end();
   });
 }
 
@@ -21,6 +48,11 @@ app.post('/post/16', handlePost);
 app.post('/post/64', handlePost);
 app.post('/post/256', handlePost);
 app.post('/post/1024', handlePost);
+
+app.get('/get/16', curriedGetResponse('16'));
+app.get('/get/64', curriedGetResponse('64'));
+app.get('/get/256', curriedGetResponse('256'));
+app.get('/get/1024', curriedGetResponse('1024'));
 
 app.listen(3000, function() {
   console.log('File server listening on port 3000');
